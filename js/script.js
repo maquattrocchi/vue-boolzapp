b4b93196d2b2f40ad924f1bc60ca5a9f225457a9
maquattrocchi <martinaquattrocchi5@gmail.com>
2022-04-26 17:06:52 +0200

    index+style: struttura + lista chat

---
+++ js/script.js
@@ -0,0 +1,192 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        contacts: [
+            {
+                nome: 'Ge',
+                avatar: '_1',
+                visible: true,
+                messages: [
+                    {
+                        date: '15:30',
+                        message: 'Vuoi venire al cinema stasera?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:33',
+                        message: 'A vedere cosa?',
+                        status: 'received'
+                    },
+                    {
+                        date: '15:37',
+                        message: 'Non so decidiamo sul momento',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                nome: 'Ale',
+                avatar: '_2',
+                visible: false,
+                messages: [
+                    {
+                        date: '15:30',
+                        message: 'Ehii, ci sei al compleanno di Luca',
+                        status: 'received'
+                    },
+                    {
+                        date: '15:33',
+                        message: 'Sii certo',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:37',
+                        message: 'Oh fantastico, non riesco a decidere cosa mettere',
+                        status: 'received'
+                    },
+                    {
+                        date: '15:33',
+                        message: 'Ah guarda non ne ho idea, parliamo con gli altri e vediamo',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                nome: 'Peppe',
+                avatar: '_3',
+                visible: true,
+                messages: [
+                    {
+                        date: '15:30',
+                        message: 'Vuoi venire al cinema stasera?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:33',
+                        message: 'A vedere cosa?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:37',
+                        message: 'Non so decidiamo sul momento',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                nome: 'Ori',
+                avatar: '_4',
+                visible: true,
+                messages: [
+                    {
+                        date: '15:30',
+                        message: 'Vuoi venire al cinema stasera?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:33',
+                        message: 'A vedere cosa?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:37',
+                        message: 'Non so decidiamo sul momento',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                nome: 'Myxi',
+                avatar: '_5',
+                visible: true,
+                messages: [
+                    {
+                        date: '15:30',
+                        message: 'Vuoi venire al cinema stasera?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:33',
+                        message: 'A vedere cosa?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:37',
+                        message: 'Non so decidiamo sul momento',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                nome: 'Antonio',
+                avatar: '_6',
+                visible: true,
+                messages: [
+                    {
+                        date: '15:30',
+                        message: 'Vuoi venire al cinema stasera?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:33',
+                        message: 'A vedere cosa?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:37',
+                        message: 'Non so decidiamo sul momento',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                nome: 'Giorgia',
+                avatar: '_7',
+                visible: true,
+                messages: [
+                    {
+                        date: '15:30',
+                        message: 'Vuoi venire al cinema stasera?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:33',
+                        message: 'A vedere cosa?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:37',
+                        message: 'Non so decidiamo sul momento',
+                        status: 'sent'
+                    },
+                ]
+            },
+            {
+                nome: 'Luca',
+                avatar: '_8',
+                visible: true,
+                messages: [
+                    {
+                        date: '15:30',
+                        message: 'Vuoi venire al cinema stasera?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:33',
+                        message: 'A vedere cosa?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15:37',
+                        message: 'Non so decidiamo sul momento',
+                        status: 'sent'
+                    },
+                ]
+            },
+        ],
+        currentChat: 0,
+    },
+    methods: {
+
+    }
+})
